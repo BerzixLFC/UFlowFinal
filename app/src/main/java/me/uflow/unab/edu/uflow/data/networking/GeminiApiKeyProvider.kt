95f006231a23bde632e7371768861853f0e9c026
Sierra <jsierra16@unab.edu.co>
2025-11-14 19:10:15 -0500

    Gemini: API arreglada, el chat con la IA gemini funciona

--- app/src/main/java/me/uflow/unab/edu/uflow/data/networking/GeminiApiKeyProvider.kt
+++ app/src/main/java/me/uflow/unab/edu/uflow/data/networking/GeminiApiKeyProvider.kt
@@ -1,4 +1,5 @@
 package me.uflow.unab.edu.uflow.data.networking
 object GeminiApiKeyProvider {
-    const val API_KEY = "AIzaSyAQEVPtg9sQM2mNrk7IiP8UqI8BPn5d8nM"
+
+    const val API_KEY = "AIzaSyBpXDFK-dIchhPhcME7N6yScX3EHmTCx6s"
 }
